a166b6f0aa33c47d88533cdb3df95e25ec7057e1
Conrad Mitchell <conradmitchell@outlook.com>
2026-01-02 17:25:03 +0000

    Change default page to Map

--- TFL_API/Program.cs
+++ TFL_API/Program.cs
@@ -51,7 +51,7 @@ namespace TFL_API
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Map}/{id?}");
 
             app.Run();
         }
